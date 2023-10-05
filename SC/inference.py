aa35ed9f717536439fea9cc5604c2752cc8565cd
Taewan Cho <taewan2002@gachon.ac.kr>
2023-10-05 21:26:27 +0900

    fix: inference 배치사이즈 수정

--- SC/inference.py
+++ SC/inference.py
@@ -18,15 +18,24 @@ parser = argparse.ArgumentParser(prog="inference", description="Inference")
 parser.add_argument("--geneartion-model-ckpt-path", type=str, default="outputs/generation",help="generation model path")
 parser.add_argument("--validation-model-ckpt-path", type=str, default="outputs/validation",help="validation model path")
 parser.add_argument("--adapter-model-ckpt-path", type=str, default="outputs/adapter",help="adapter model path")
-parser.add_argument("--base_model", type=str, default="nlpai-lab/kullm-polyglot-5.8b-v2")
+parser.add_argument("--k", type=int, default=3)
+parser.add_argument("--batch_size", type=int, default=8)
 
-def infer(instruction, input_text, pipe, prompter):
-        prompt = prompter.generate_prompt(instruction, input_text)
-        output = pipe(prompt, max_length=256, temperature=0.2, num_beams=3, eos_token_id=2)
-        s = output[0]["generated_text"]
-        result = prompter.get_response(s)
+def infer_batch(instructions, input_texts, model, tokenizer, prompter, device='cuda'):
+    prompts = [prompter.generate_prompt(instruction, input_text) for instruction, input_text in zip(instructions, input_texts)]
+    inputs = tokenizer(prompts, return_tensors="pt", padding=True, truncation=True, max_length=256).to(device)
+    
+    # Remove token_type_ids if present
+    if 'token_type_ids' in inputs:
+        del inputs['token_type_ids']
 
-        return result
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=256, temperature=0.2, num_beams=3, eos_token_id=2)
+    
+    decoded_outputs = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
+    results = [prompter.get_response(s) for s in decoded_outputs]
+    
+    return results
 
 def merge_LoRA(BASE_MODEL, adapter, save_path):
     base_model = GPTNeoXForCausalLM.from_pretrained(
@@ -61,9 +70,7 @@ def inference(args):
     logger = get_logger("inference")
     BASE_MODEL = args.base_model
     tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
-    k = 3
-
-    for i in tqdm(range(1, k+1), desc="Processing folds"):
+    for i in tqdm(range(1, args.k+1), desc="Processing folds"):
         geneartion_adapter = args.adapter_model_ckpt_path + f"/fold_generation_{i}"
         validation_adapter = args.adapter_model_ckpt_path + f"/fold_validation_{i}"
 
@@ -129,29 +136,35 @@ def inference(args):
         
         model.eval()
 
-        pipe = pipeline("text-generation", model=model, tokenizer=MODEL, device=0)
+        # pipe = pipeline("text-generation", model=model, tokenizer=MODEL, device=0)
         prompter = Prompter("kullm")
         logger.info("[+] Load Dataset")
-        print(result_file_name)
         test_data = load_dataset("json", data_files=result_file_name)
         test_data = test_data["train"]
        
-        result_file_name = f"output/files/validate_results_{i}.jsonl"
+        result_file_name = f"outputs/files/validate_results_{i}.jsonl"
         all_output_data_points = []  # 결과를 저장할 리스트
 
-        for i, data_point in enumerate(tqdm(test_data, desc="Processing")):  # tqdm을 사용하여 진행 상태를 표시
-            instruction = "문맥과 문법적 정확성 및 논리적 일관성에 맞는 자연스러운 한 문장이 되도록 두 문장 이후에 나올 한 문장을 접속사를 신경써서 만들어주세요."
-            input_text = f"{data_point['input']['sentence1']}{data_point['output']}"
+        BATCH_SIZE = args.batch_size
+        instructions = ["두 문장 뒤에 나올 한 문장을 만들어주세요."] * BATCH_SIZE
 
-            result = infer(instruction, input_text, pipe, prompter)
+        for i in tqdm(range(0, len(test_data), BATCH_SIZE)):
+            batch_data = [test_data[j] for j in range(i, min(i+BATCH_SIZE, len(test_data)))]
+                    
+            input_texts = [f"{data_point['input']['sentence1']}{data_point['output']}" for data_point in batch_data]
+            
+            results = infer_batch(instructions[:len(input_texts)], input_texts, model, tokenizer, prompter)
 
-            output_data_point = {
-                "id": data_point["id"],
-                "input": data_point["input"]['sentence3'],
-                "output": result
-            }
-
-            all_output_data_points.append(output_data_point)  # 메모리에 결과 저장
+            for data_point, result in zip(batch_data, results):
+                output_data_point = {
+                    "id": data_point["id"],
+                    "input": {
+                        "sentence1": data_point["input"]['sentence1'],
+                        "sentence2": data_point['output']
+                    },
+                    "output": result
+                }
+                all_output_data_points.append(output_data_point)
 
         with open(result_file_name, "w") as f:
             for output_data_point in all_output_data_points:
